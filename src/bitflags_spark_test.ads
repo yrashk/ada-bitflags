3e9c11a3199cdc3e5b1c2fb17e0393b5cefd2bf0
Yurii Rashkovskii <yrashk@gmail.com>
2024-07-18 12:12:16 -0700

    Problem: SPARK test doesn't show intended use
    It relies on default enum representation. That's not how people would be
    using this.
    
    Solution: be explicit about the representation

--- src/bitflags_spark_test.ads
+++ src/bitflags_spark_test.ads
@@ -5,7 +5,10 @@ is
 
    type Option is (Test, This) with
      Size => 8;
+   for Option use (Test => 2#01#, This => 2#10#);
+
    type Flags_Type is mod 2**8 with
      Size => 8;
+
    package Test_Flag is new Bitflags (Flags_Type => Flags_Type, Option_Type => Option);
 end Bitflags_SPARK_Test;
